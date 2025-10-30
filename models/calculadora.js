e9f078f416d96c446ea47250c3eab16e5190e186
DIEGO RODRIGO BARBOSA VITAL <diego_vital@msn.com>
2025-10-30 11:51:14 -0300

    endpoint /api/v1/status | integratio test

--- models/calculadora.js
+++
@@ -1,7 +0,0 @@
-function somar(num1, num2) {
-  let soma = 0;
-  soma += num1 + num2;
-  return soma;
-}
-
-exports.somar = somar;
--- tests/calculadora.test.js
+++
@@ -1,5 +0,0 @@
-const calculadora = require("../models/calculadora.js");
-
-test("testa a função soma", () => {
-  expect(calculadora.somar(1, 2)).toBe(3);
-});
