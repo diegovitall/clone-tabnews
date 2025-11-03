89648cd91fca2e4e2beb76e0e2159849cff4b563
Diego Rodrigo Barbosa Vital <diego_vital@msn.com>
2025-11-03 14:57:15 -0300

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,33 @@
 import database from "infra/database.js";
 
-async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({chave: "alunos do curso.dev são pessoas acima da média"});
+
+
+async function status(request, response) { 
+  const updatedAt = new Date().toISOString();
   
+  const postgresVersion = await database.query("SHOW server_version;");
+  const postgresVersionValue = postgresVersion.rows[0].server_version;
+  
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName]
+  });
+  const openedConnectionsValue = openedConnections.rows[0].count;
+
+
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      postgres_version: postgresVersionValue,
+      max_connections: parseInt(maxConnectionsValue),
+      opened_connections: openedConnectionsValue
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const postgresVersion = await responseBody.dependencies.postgres_version;
+  expect(postgresVersion).toMatch(/^[\d\.]+$/);
+
+  const maxConnections = await responseBody.dependencies.max_connections;
+  expect(maxConnections).toEqual(100);
+
+  const openedConnections = await responseBody.dependencies.opened_connections;
+  expect(openedConnections).toEqual(1);
 });
