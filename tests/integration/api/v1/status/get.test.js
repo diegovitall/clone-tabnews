8315066bb4e12561b762afe8323f970ca536a5fb
DIEGO RODRIGO BARBOSA VITAL <diego_vital@msn.com>
2025-11-28 08:59:04 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,21 +4,26 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  const postgresVersion = await responseBody.dependencies.postgres_version;
-  expect(postgresVersion).toMatch("16.0");
+      const postgresVersion = await responseBody.dependencies.postgres_version;
+      expect(postgresVersion).toMatch("16.0");
 
-  const maxConnections = await responseBody.dependencies.max_connections;
-  expect(maxConnections).toEqual(100);
+      const maxConnections = await responseBody.dependencies.max_connections;
+      expect(maxConnections).toEqual(100);
 
-  const openedConnections = await responseBody.dependencies.opened_connections;
-  expect(openedConnections).toEqual(1);
+      const openedConnections =
+        await responseBody.dependencies.opened_connections;
+      expect(openedConnections).toEqual(1);
+    });
+  });
 });
